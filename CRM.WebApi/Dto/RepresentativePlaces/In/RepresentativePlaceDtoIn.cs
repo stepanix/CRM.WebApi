4954e7f4c7fe80b6bbde5d0aa1831447418a0cfb
Henry Oforeh <henry.oforeh@standardbank.co.za>
2017-09-02 12:01:42 +0200

    Updates

--- CRM.WebApi/Dto/RepresentativePlaces/In/RepresentativePlaceDtoIn.cs
+++ CRM.WebApi/Dto/RepresentativePlaces/In/RepresentativePlaceDtoIn.cs
@@ -4,7 +4,6 @@ namespace CRM.WebApi.Dto.RepresentativePlaces.In
 {
     public class RepresentativePlaceDtoIn
     {
-        public int Id { get; set; }
         public string UserId { get; set; }       
         public int PlaceId { get; set; }
     }
